3e61d9eb3741749d523656421a87e63f5a8b85a2
minami-cs <mina9301@gmail.com>
2024-06-11 15:52:13 +0900

    feat: logical route group 만드는 방법 실습

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,9 @@
 // next는 실행시 무조건 app 폴더 안의 page.tsx를 먼저 참조한다
 // 여기가 root
+
+// (폴더명)은 url 경로를 생성하지 않는다!
+// 하지만 page가 동작하는 것에는 아무런 문제가 없음!
+// (폴더명): 논리적인 route group을 만드는 방법
 export default function Page() {  // function naming은 무엇이든 상관없음
     return (
         <div>

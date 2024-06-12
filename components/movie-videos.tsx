81797d77a7a4f5b8720740b7d37f065a69e40b80
minami-cs <mina9301@gmail.com>
2024-06-12 14:03:58 +0900

    feat: movie-video 컴포넌트에 styles 적용

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { MOVIES_BASE_URL } from "../constants/constants";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {
   // error handling을 위해서 일부러 에러 발생
@@ -14,5 +15,17 @@ async function getVideos(id: string) {
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
 
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      ))}
+    </div>
+  );
 }
